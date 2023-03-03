26c955a5fb622c748322e4e327c10927063f7530
michael-lally <mplally@neiu.edu>
2023-03-03 22:15:29 +0000

    Completed  Average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,9 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+       double avg = 0;
+        for(int i = 0 ; i< nums.length; i++)
+            avg += nums[i];
+        return avg/nums.length ;
     }
 }
